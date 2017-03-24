7b6297c89858c333d86e80b2bfcf99425727194e
mattroz <matveyrozanov@gmail.com>
2017-03-24 09:49:20 +0000

    Changed definitions according to the header

--- RCIP/rcip.c
+++ RCIP/rcip.c
@@ -6,7 +6,7 @@
 /*	Define instruction packet wrapping. 
  *	Since we only need to wrap instruction packets,
  *	there is no need to write the same function for data packets	*/
-int wrap_instr_packet(rcip_instr_pack_t **packet, int _instr, int _pow)
+int wrap_instr_packet(rcip_instr_pack_t **packet, int leftval, int rightval)
 {
 	/*	allocate memory for a packet and handle allocation error	*/
 	*(packet) = malloc(sizeof(rcip_instr_pack_t));
@@ -15,8 +15,8 @@ int wrap_instr_packet(rcip_instr_pack_t **packet, int _instr, int _pow)
 		return -1;		
 	}
 
-	(*packet)->instr = _instr;
-	(*packet)->pow = _pow;	
+	(*packet)->left_engine = leftval;
+	(*packet)->right_engine = rightval;	
 	
 	return 0;
 }
