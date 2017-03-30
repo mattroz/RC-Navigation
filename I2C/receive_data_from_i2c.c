d3e31d222cd9361376eb7598030a14bb27c308c3
mattroz <matveyrozanov@gmail.com>
2017-03-30 12:06:14 +0000

    Receive data from PC104 via I2C using contexts

---
+++ I2C/receive_data_from_i2c.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+
+#include "../RCC/rccore.h"
+#include "../RCIP/rcip.h"
+
+int main()
+{
+    RPiContext *RPi;
+    PC104Context *PC104;
+
+    /*  initialize contexts */
+    if(rpi_init(&RPi) == -1)
+    {
+        perror("Failed to initialize Raspberry Pi context");
+        exit(EXIT_FAILURE);
+    }
+
+    if(pc104_init(&PC104) == -1)
+    {
+        perror("Failed to initialize PC104 context");
+        exit(EXIT_FAILURE);
+    }
+
+    /*  open I2C connection */
+    int file_i2c;
+    if ((file_i2c = open(RPi->i2c_path, O_RDWR)) < 0)
+    {
+        perror("Failed to open the i2c bus");
+        exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(file_i2c, I2C_SLAVE, PC104->i2c_slave_addr) < 0)
+    {
+        perror("Failed to access an i2c slave");
+        exit(EXIT_FAILURE);
+    }
+
+	/*	receive data from I2C	*/
+	int length = 4;
+	unsigned char *buffer = malloc(sizeof(unsigned char) * length);
+	while(1)
+	{
+		if(read(file_i2c, buffer, length) != length)
+		{
+			perror("Failed to read from i2c");
+			exit(EXIT_FAILURE);
+		}	
+		unsigned long numerical_part = atoi(buffer);
+		printf("Data read: %lu\n", numerical_part);
+	}
+
+    return 0;
+}
