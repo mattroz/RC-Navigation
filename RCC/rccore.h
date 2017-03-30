7837fda2cea04c3076c39d003b955f2564f281f8
mattroz <matveyrozanov@gmail.com>
2017-03-30 10:53:55 +0000

    Added default defines, initializers and destructors declarations

--- RCC/rccore.h
+++ RCC/rccore.h
@@ -3,12 +3,20 @@
 
 #include <stdint.h>
 
+#define RPI_VERSION 3
+#define PC104_VERSION 1
+
+/*	define default settings	*/
+#define DEFAULT_I2C_PATH "/dev/i2c-1"
+#define DEFAULT_PIPE_PATH "/var/run/raspi_pipe"
+#define DEFAULT_PC104_I2C_SLAVE_ADDR 0x02
+
 /*	Raspberry Pi context declaration	*/
 typedef struct RPiContext
 {
-	uint8_t version,
-	char[512] i2c_path,
-	char[512] pipe_path
+	uint8_t version;
+	char *i2c_path;
+	char *pipe_path;
 } RPiContext;
 
 
@@ -16,8 +24,14 @@ typedef struct RPiContext
 typedef struct PC104Context
 {
 	uint8_t version;
-	char[512] i2c_slave_addr
+	uint16_t i2c_slave_addr;
 } PC104Context;
 
+
+/*	contexts initializers and destructors declaration	*/
+int rpi_init(RPiContext**);
+void rpi_destruct(RPiContext*);
+int pc104_init(PC104Context**);
+void pc104_destruct(PC104Context*);
 
 #endif
