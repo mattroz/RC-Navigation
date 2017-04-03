bed46b04d479801bc1ac5b969be00c0bc90e965a
mattroz <matveyrozanov@gmail.com>
2017-04-03 15:04:40 +0000

    Error handling, added error values returning according to defined error in rccore.h

--- RCC/rccore.c
+++ RCC/rccore.c
@@ -3,19 +3,21 @@
 
 #include "rccore.h"
 
+
 /*Raspberry Pi context initializer and destructor definition*/
 int rpi_init(RPiContext **rpi)
 {
 	*rpi = malloc(sizeof(RPiContext));
-	if(*rpi == NULL)	return -1;
+	if(*rpi == NULL)	return RC_EALLOC;
 
 	(*rpi)->identifier = _RASPBERRY_;	
 	(*rpi)->version = RPI_VERSION;
 	(*rpi)->i2c_path = DEFAULT_I2C_PATH;
 	(*rpi)->pipe_path = DEFAULT_PIPE_PATH;	
 		
-	return 0;
+	return RC_SUCCESS;
 }
+
 
 void rpi_destruct(RPiContext *rpi)
 {
@@ -27,13 +29,13 @@ void rpi_destruct(RPiContext *rpi)
 int pc104_init(PC104Context **pc104)
 {
 	*pc104 = malloc(sizeof(PC104Context));
-	if(*pc104 == NULL) return -1;
+	if(*pc104 == NULL) return RC_EALLOC;
 	
 	(*pc104)->identifier = _PC104_;
 	(*pc104)->version = PC104_VERSION;
 	(*pc104)->i2c_slave_addr = DEFAULT_PC104_I2C_SLAVE_ADDR;	
 	
-	return 0;
+	return RC_SUCCESS;
 }
 
 
@@ -43,25 +45,10 @@ void pc104_destruct(PC104Context *pc104)
 }
 
 
-/*	contexts' error handling	
-void rc_err_handle(void *context)
+/*	Error handling	*/
+void rcerror(int errcode)
 {
-	/*	check if this pointer not NULL	
-	if(context == NULL)
-	{
-		perror("Context haven't been initialized");
-		exit(EXIT_FAILURE);
-	}	
-
-	/*	check if we've been given either Raspberry or PC104 context	
-	if(((RPiContext*)context)->identifier == _RASPBERRY_)
-	{
-		uint8_t errval = ((RPiContext*)context)->last_error;
-		fprintf(stderr, "%s:", ,errval);					
-	}
-	else if(((PC104Context*)context)->identifier == _PC104_)
-	{
-	
-	}
+	fprintf(stderr, "%s, error code: %d\n", RCErrorMessage[errcode], errcode);
+	exit(EXIT_FAILURE);
 }	
-*/		
+		
