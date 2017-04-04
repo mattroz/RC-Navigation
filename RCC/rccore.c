66c319dfc4e46643040fa10fcfdd682a6b4797e1
mattroz <matveyrozanov@gmail.com>
2017-04-04 08:45:14 +0000

    Implemented and tested rcerror() function. Works well but need to refactor it

--- RCC/rccore.c
+++ RCC/rccore.c
@@ -8,7 +8,8 @@
 int rpi_init(RPiContext **rpi)
 {
 	*rpi = malloc(sizeof(RPiContext));
-	if(*rpi == NULL)	return RC_EALLOC;
+	
+	if(*rpi == NULL) return RC_EINIT;
 
 	(*rpi)->identifier = _RASPBERRY_;	
 	(*rpi)->version = RPI_VERSION;
@@ -29,7 +30,7 @@ void rpi_destruct(RPiContext *rpi)
 int pc104_init(PC104Context **pc104)
 {
 	*pc104 = malloc(sizeof(PC104Context));
-	if(*pc104 == NULL)	return RC_EALLOC;
+	if(*pc104 == NULL)	return RC_EINIT;
 	
 	(*pc104)->identifier = _PC104_;
 	(*pc104)->version = PC104_VERSION;
@@ -46,23 +47,48 @@ void pc104_destruct(PC104Context *pc104)
 
 
 /*	Error handling	*/
-void rcerror(RCErrorContext *error_context, void* cuplrit, int errcode)
+void rcerror(RCErrorContext *error_context, void* culprit, int errcode, int status)
 {	
-	if(error_context == NULL) return RC_EINIT;
+	/*	if we catch initialization error, we can't spot which context have failed 
+		('cause we have NULL pointers instead of context pointers, thats why this 
+		 error is hadling separately)	*/
+	if(errcode == RC_EINIT)
+	{
+		culprit = NULL;
+		status = RC_EXIT;
+	}
+	
+	if(error_context == NULL)
+	{
+		fprintf(stderr, "Error context haven't been initialized, abort");
+		exit(EXIT_FAILURE);
+	}
 	
 	/*	check which context	caused an error	*/
-	if(((RPiContext*)culprit)->identifier == _RASPBERRY_)
+	if((culprit != NULL) && (((RPiContext*)culprit)->identifier == _RASPBERRY_))
 	{
 		error_context->culprit_context = _RASPBERRY_;
 	}
-	else if(((PC104Context*)context)->identifier == _PC104_)
+	else if((culprit != NULL) && (((PC104Context*)culprit)->identifier == _PC104_))
 	{
 		error_context->culprit_context = _PC104_;		
+	}
+	else
+	{
+		error_context->culprit_context = "UNKNOWN";
 	}
 	
 	error_context->last_error_code = errcode;
 	error_context->last_error_message = RCErrorMessage[errcode];
-
-	fprintf(stderr, "%s, error code: %d\n", RCErrorMessage[errcode], errcode);
+	
+	puts("====== ERROR ======");
+	fprintf(stderr, "Message: %s\nContext: %s\nError code: %d\n", 
+			RCErrorMessage[errcode], error_context->culprit_context, errcode);
+	puts("===================");
+	
+	if(status == RC_EXIT) 
+	{	
+		exit(EXIT_FAILURE);
+	}
 }
 		
