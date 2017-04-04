70a1c048396a8bba6bf92f643f9ea106d24ab58f
mattroz <matveyrozanov@gmail.com>
2017-04-04 07:06:25 +0000

    Added I2C error codes and messages

--- RCC/rccore.h
+++ RCC/rccore.h
@@ -51,8 +51,9 @@ typedef struct RPiContext
 {
 	uint8_t version;
 	char *identifier;
+	uint16_t distance_from_USRF_sensor;
 	char *i2c_path;
-	uint16_t i2c_bus_descriptor,
+	int i2c_bus_descriptor;
 	char *pipe_path;
 	uint8_t last_error;	
 } RPiContext;
@@ -64,7 +65,8 @@ typedef struct PC104Context
 	uint8_t version;
 	char *identifier;
 	uint16_t i2c_slave_addr;
-	uint16_t i2c_bus_descriptor,
+	int i2c_bus_descriptor;
+	uint16_t distance_from_IR_sensor;
 	uint8_t last_error;
 } PC104Context;
 
@@ -75,12 +77,13 @@ typedef enum RCErrorCode
 	RC_SUCCESS,		/*	return on success	*/
 	RC_EINIT,		/*	initialization error	*/
 	RC_EALLOC,		/*	memory allocation error	*/
-	RC_EPIPE_CREATE,	/*	pipe creating error	*/		
-	RC_EPIPE_READ,		/*	error reading value from pipe	*/
-	RC_EPIPE_WRITE,		/*	error writing value to pipe	*/		
+	RC_PIPE_ECREATE,	/*	pipe creating error	*/		
+	RC_PIPE_EREAD,		/*	error reading value from pipe	*/
+	RC_PIPE_EWRITE,		/*	error writing value to pipe	*/		
 	RC_I2C_EOPEN,	/*	error opening i2c connection	*/
 	RC_I2C_EWRITE,	/*	error writing to slave via i2c	*/
 	RC_I2C_EREAD	/*	error reading value from i2c	*/
+	RC_I2C_ESLAVE	/*	slave detection error	*/
 } rc_errcode_t;
 
 
@@ -89,9 +92,14 @@ static char *RCErrorMessage[] = {
 	"Initialization succeed",		/*	RC_SUCCESS	*/
 	"Initialization failed",		/*	RC_EINIT	*/
 	"Failed to allocate memory",		/*	RC_EALLOC	*/
-	"Failed creating pipe",			/*	RC_EPIPE_CREATE	*/
-	"Failed reading value from pipe",	/*	RC_EPIPE_READ	*/
-	"Failed writing value to pipe"		/*	RC_EPIPE_WRITE	*/
+	"Failed creating pipe",			/*	RC_PIPE_ECREATE	*/
+	"Failed reading value from pipe",	/*	RC_PIPE_EREAD	*/
+	"Failed writing value to pipe",		/*	RC_PIPE_EWRITE	*/
+	"Failed to access the i2c bus ",	/*	RC_I2C_EOPEN	*/
+	"Failed to send data via i2c",	/*	RC_I2C_EWRITE	*/
+	"Failed to receive data via i2c",	/*	RC_I2C_EREAD	*/
+	"Failed to access an i2c slave"	/*	RC_I2C_ESLAVE	*/
+
 };
 
 /*	contexts initializers and destructors declaration	*/
