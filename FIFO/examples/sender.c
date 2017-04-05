938d29ec46995ee79ea7b4fffb9609efc5a5d85b
mattroz <matveyrozanov@gmail.com>
2017-04-05 06:32:05 +0000

    Added Makefile, changed examples

--- FIFO/examples/sender.c
+++ FIFO/examples/sender.c
@@ -6,28 +6,47 @@
 #include <sys/types.h>
 #include <fcntl.h>
 #include <errno.h>
-#include "rppipe.h"
 
+#include "../rppipe.h"
+#include "../../RCC/rccore.h"
 
-int main(void)
+int main()
 {
-	if(create_pipe() != EXIT_SUCCESS)
+	RPiContext *RPi;
+	PC104Context *PC104;
+	RCErrorContext *errcont = malloc(sizeof(RCErrorContext));
+
+	/*  initialize contexts */
+	if(rpi_init(&RPi) == RC_EINIT)
 	{
-		perror("Failed to create pipe");
-		exit(EXIT_FAILURE);	
+		rcerror(errcont, RPi, RC_EINIT, RC_EXIT);
+	}
+
+	if(pc104_init(&PC104) == RC_EINIT)
+	{
+		rcerror(errcont, PC104, RC_EINIT, RC_EXIT);
+	}
+
+	/*	create pipe	*/
+	if(create_pipe(RPi) != RC_SUCCESS)
+	{
+		rcerror(errcont, RPi, RPi->last_error, RC_EXIT);
 	}
 
 	int i = 0;
 	while(1)
 	{
-		if(write_value_to_pipe(i++) == EXIT_FAILURE)
+		if(write_value_to_pipe(RPi, i++) != RC_SUCCESS)
 		{
-			perror("Failed to write value to pipe\n");
-			exit(EXIT_FAILURE);
+			rcerror(errcont, RPi, RPi->last_error, RC_EXIT);
 		}
 		usleep(1000);
 	}
 
+	pc104_destruct(PC104);
+	rpi_destruct(RPi);
+	free(errcont);
+	
 	return 0;
 }
 
