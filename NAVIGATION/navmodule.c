840a0fd8112c3a03beae6960f9972e3052d692b7
mattroz <matveyrozanov@gmail.com>
2017-04-18 13:46:05 +0000

    Added steering functions, added constraint for minimal distance to 2 cm

--- NAVIGATION/navmodule.c
+++ NAVIGATION/navmodule.c
@@ -1,10 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 #include "../RCC/rccore.h"
 #include "../I2C/rci2c.h"
 #include "../SENSORS/rcsensors.h"
+
+#define ENG_VAL 9
+
+
+int left(RPiContext*);
+int right(RPiContext*);
+int backward(RPiContext*);
+int forward(RPiContext*);
+int stop(RPiContext*);
+
+
+enum EDirections
+{
+	FORWARD = 11,
+	BACKWARD = 00,
+	LEFT = 10,
+	RIGHT = 01,
+	STOP = -1
+};
 
 
 int main(int argc, char **argv)
@@ -40,21 +60,27 @@ int main(int argc, char **argv)
 	/*	INITIALIZATION END	*/
 		
 
+	int last_command = STOP;
 	/*	receive data from ultrasonic sensor and change steering vector	*/
-	while(1)	
+	while((RPi->distance_from_USRF_sensor = get_distance_in_cm()) > 2)	
 	{
-		RPi->distance_from_USRF_sensor = get_distance_in_cm();
 		if(RPi->distance_from_USRF_sensor < 15)
 		{
-			if(send_to_slave_via_i2c(RPi, 0, 0) == RC_SUCCESS)
-				puts("STOP");
+			if(last_command != STOP)
+			{
+				stop(RPi);
+				last_command = STOP;
+			}
 		}
 		else
 		{
-			if(send_to_slave_via_i2c(RPi, 7, 7) == RC_SUCCESS)
-				puts("FORWARD");
+			if(last_command != FORWARD)
+			{
+				forward(RPi);
+				last_command = FORWARD;
+			}
 		}
-		usleep(5000);
+		usleep(500);
 	}
 	
 	close(RPi->i2c_bus_descriptor);
@@ -63,4 +89,35 @@ int main(int argc, char **argv)
 	free(errcontext);
 
 	return 0;
+}
+
+
+int forward(RPiContext* rpi)
+{
+	puts("FORWARD");
+	return send_to_slave_via_i2c(rpi, ENG_VAL, ENG_VAL);	
+}
+
+int backward(RPiContext *rpi)
+{
+	puts("BACKWARD");
+	return send_to_slave_via_i2c(rpi, -ENG_VAL, -ENG_VAL);
+}
+
+int left(RPiContext *rpi)
+{	
+	puts("LEFT");
+	return send_to_slave_via_i2c(rpi, 0, ENG_VAL);
+}
+
+int right(RPiContext *rpi)
+{
+	puts("RIGHT");
+	return send_to_slave_via_i2c(rpi, ENG_VAL, 0);
+}
+
+int stop(RPiContext *rpi)
+{
+	puts("STOP");
+	return send_to_slave_via_i2c(rpi, 0, 0);
 }
