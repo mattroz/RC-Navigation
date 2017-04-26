fb613c37ea84e1f479522eb78c32484db0b0ac13
mattroz <matveyrozanov@gmail.com>
2017-04-26 11:10:29 +0300

    Changed example for initizalizing and destructing according to changes in rccore.c

--- RCC/examples/err_handling.c
+++ RCC/examples/err_handling.c
@@ -7,23 +7,27 @@ int main()
 {
 	RPiContext *RPi;
 	PC104Context *PC104;
-	RCErrorContext *errcont = malloc(sizeof(RCErrorContext));
-	/*
+	RCErrorContext *errcont;
+	
+	if(error_context_init(&errcont) == RC_EINIT)
+	{
+		rcerror(errcont, NULL, RC_EXIT);
+	}
+
 	if(rpi_init(&RPi) == RC_EINIT)
 	{
-		rcerror(errcont, RPi, RC_EINIT, RC_EXIT);
-	}*/
+		rcerror(errcont, RPi, RC_EXIT);
+	}
 	
 	if(pc104_init(&PC104) == RC_EINIT)
 	{
+		rpi_destruct(RPi);
 		rcerror(errcont, PC104, RC_EXIT);
 	}
-
-	rcerror(errcont, RPi, RC_EXIT);
 	
 	rpi_destruct(RPi);
 	pc104_destruct(PC104);
-	free(errcont);
+	error_context_destruct(errcont);
 
 	
 	return 0;
