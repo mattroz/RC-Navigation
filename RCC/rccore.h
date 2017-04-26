54f853a2433e30375d12c89bc8f29f9d7ac9afad
mattroz <matveyrozanov@gmail.com>
2017-04-26 14:09:12 +0300

    Added battery voltage field (in mV)

--- RCC/rccore.h
+++ RCC/rccore.h
@@ -81,6 +81,7 @@ typedef struct PC104Context
 	uint16_t i2c_slave_addr;
 	int i2c_bus_descriptor;
 	unsigned long distance_from_IR_sensor;
+	uint16_t battery_voltage_mV;
 	uint8_t last_error;
 } PC104Context;
 
